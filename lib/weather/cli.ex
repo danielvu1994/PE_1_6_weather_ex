3c17ba97413066bac3f6c99bcfca9ade74f54f86
danielvu1994 <vulinhdam1994@gmail.com>
2021-04-04 00:26:21 +0700

    Handle multiple options & wrong input

--- lib/weather/cli.ex
+++ lib/weather/cli.ex
@@ -1,15 +1,29 @@
 defmodule Weather.CLI do
   import Weather.TableFormatter, only: [ print_table_for_columns: 2 ]
-  import Weather.CrawData, only: [fetch_data: 0]
+  import Weather.CrawData, only: [fetch_data: 1]
 
-  def main() do
-    fetch_data()
-    |> get_useful_data
-    # |> IO.inspect
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts """
+    useage: weather <location_id_1> <location_id_2> ...
+    """
+  end
+  def process(location_list) do
+    location_list
+    |> Enum.map(&fetch_data(&1))
+    |> Enum.map(&get_useful_data(&1))
     |> print_table_for_columns(["station_id", "location", "temp_c", "wind_degrees"])
   end
 
-  def get_useful_data({:error, _}), do: {:error, "Sorry!! There's no data"}
+  def get_useful_data({:error, location_id})  do
+    %{"station_id" => location_id, "location" => "Err.No such id",
+      "temp_c" => "--------", "wind_degrees" => "-------" }
+  end
   def get_useful_data({:ok, map}) do
     %{"current_observation" => current_observation} = map
     %{"#content" => content} = current_observation
@@ -17,6 +31,19 @@ defmodule Weather.CLI do
     #   "temp_c" => temp_c, "wind_degrees" => wind_degrees } = content
     # [station_id, location, temp_c, wind_degrees]
 
-    [content]
+    content
   end
+
+  def parse_args(argv) do
+    OptionParser.parse(argv, switches: [ help: :boolen],
+                             aliaes: [h: :help])
+    |> elem(1)
+    |> args_to_location_list
+  end
+
+  def args_to_location_list([]) do
+    :help
+  end
+  def args_to_location_list(list), do: list
+
 end
--- lib/weather/craw_data.ex
+++ lib/weather/craw_data.ex
@@ -1,19 +1,23 @@
 defmodule Weather.CrawData do
   @user_agent  [ {"User-agent", "daniel"}]
 
-  def fetch_data() do
-    url = "https://w1.weather.gov/xml/current_obs/KDTO.xml"
+  def fetch_data(loction_Id) do
+    url = "https://w1.weather.gov/xml/current_obs/#{loction_Id}.xml"
 
     HTTPoison.get(url, @user_agent)
-    |> IO.inspect
-    |> handle_response()
-    |> IO.inspect
+    |> handle_response(loction_Id)
   end
 
-  def handle_response({ :ok, %{status_code: status_code, body: body}}) do
+  def handle_response({ :ok, %{status_code: status_code, body: body}},loction_Id) do
     #Return tupple
+    status = status_code |> check_for_error()
+    handle_response_2(status, body, loction_Id)
+  end
+
+  def handle_response_2(:error, _body, loction_Id), do: {:error, loction_Id}
+  def handle_response_2(:ok, body, _loction_Id) do
     {
-      status_code |> check_for_error(),
+      :ok,
       body |> XmlToMap.naive_map()
     }
   end
